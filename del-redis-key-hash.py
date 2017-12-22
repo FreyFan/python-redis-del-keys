5553fc6d6f80145080bce7f54c438809f5d6acc1
Frey.Fan <863083337@qq.com>
2017-12-22 10:21:25 +0800

    修改代码缩进
    修改代码缩进

--- del-redis-key-hash.py
+++ del-redis-key-hash.py
@@ -1,17 +1,16 @@
 import redis
 
-def del_hkeys(pattern) : 
-cnt = redis.Redis(host=’xxxx’,port=6379,db=0) 
-key_dict={} 
-keys = cnt.keys(pattern); 
-for key in keys: 
-subkeys = cnt.hkeys(key) 
-key_dict[key] = subkeys 
-for key in key_dict.keys(): 
-subkeys = key_dict[key]
-
-    for subkey in subkeys:
-        if str(subkey).__contains__("http://report"):
-            print subkey
-            cnt.hdel(key, subkey)
-del_hkeys(“crawler_distinct_redis_setname”)
+def del_hkeys(pattern) :
+    cnt = redis.Redis(host='xxx',port=6379,db=0)
+    key_dict={}
+    keys = cnt.keys(pattern);
+    for key in keys:
+        subkeys = cnt.hkeys(key)
+        key_dict[key] = subkeys
+        for key in key_dict.keys():
+            subkeys = key_dict[key]
+            for subkey in subkeys:
+                if str(subkey).__contains__("http://report"):
+                    print subkey
+                    cnt.hdel(key, subkey)
+del_hkeys("crawler_distinct_redis_setname")
